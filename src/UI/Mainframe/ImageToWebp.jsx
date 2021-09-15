52657498e353bf317b33d20165faee4c28672c10
yusa-imit <esimileiar81@gmail.com>
2021-09-15 20:15:44 +0900

    confirm sharp working

--- src/UI/Mainframe/ImageToWebp.jsx
+++ src/UI/Mainframe/ImageToWebp.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React , {useState}from 'react'
 import './ImageToWebp.css'
 //import sharp from 'sharp'
-
+import {sharpConvert, sharpToFile} from './sharpFunction.js'
 export default function ImageToWebp(props){
+    const [img, setImg] = useState('');
     const availableFormat = ['jpeg', 'png','webp', 'avif','tiff', 'gif', 'heif']
     const sharp = require('sharp');
+    const imageConversion = require('image-conversion');
+    sharpConvert('C:\\Users\\esimi\\Downloads\\E8guACLVkAQV6-j.jpeg', 'webp').then(
+        buffer=>{
+            console.log(buffer)
+            //const pixelArray = new Uint8ClampedArray(buffer);
+            //console.log(pixelArray)
+            const url = imageConversion.filetoDataURL(new Blob([buffer])).then(
+                data=>{
+                    setImg(data)
+                    console.log(data)
+                }
+            )
+            sharp(new Uint8ClampedArray(buffer.buffer)).toFile('C:\\Users\\esimi\\Downloads\\E8guACLVkAQV6-j.webp').then(info=>{console.log(info)});
+        }
+    )
+    
+    
     return (
         <>
-            <div></div>
+            <div>
+                <img src={img}></img>
+            </div>
         </>
     )
 }
--- src/UI/Mainframe/sharpFunction.js
+++ src/UI/Mainframe/sharpFunction.js
@@ -1,7 +1,14 @@
 export async function sharpConvert(file, format){
     const sharp = require('sharp');
     const data = await sharp(file)
+    .withMetadata()
     .toFormat(format)
-    .toFile();
+    .toBuffer();
     return data;
+}
+
+export function sharpToFile(file){
+    const sharp = require('sharp');
+    const name = file.split('.')[0]+'converted'+'.'+file.split('1');
+    sharp(file).toFile(name);
 }
