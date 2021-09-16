b601b7df77371417da29230811fd6e554b6a713f
yusa-imit <esimileiar81@gmail.com>
2021-09-16 22:51:48 +0900

    sharp function reconfigure

--- src/UI/Mainframe/sharpFunction.js
+++ src/UI/Mainframe/sharpFunction.js
@@ -9,10 +9,15 @@ export async function sharpConvert(file, format){
 
 //이 함수는 다시 컴포넌트 안에 구현할 것
 //주고받아야 할 매개변수의 크기가 너무 큼
-export async function sharpConvertAndExport(file, format, setInfo){
+export function sharpConvertAndExport(file, format, targetDest, setInfo, increaseProgress){
     const sharp = require('sharp');
-    const imageConversion = require('image-conversion');
-    const returnFileName = file.split('.')[0]+'__converted_by_WebImage.'+format;
+    const formatChange = file.split('.')[0]+'__converted_by_WebImage.'+format;
+    const arr = formatChange.split('\\')
+    const fileName = arr.pop();
+    const originDest = arr.join('\\');
+    const target = targetDest==='Default'?originDest:targetDest;
+    const returnFileName = target + '\\'+fileName;
+    //console.log(returnFileName)
     sharpConvert(file, format).then(
         buffer=>{
             /*
@@ -23,15 +28,17 @@ export async function sharpConvertAndExport(file, format, setInfo){
                     setData(data);
                 }
             )*/
+            //console.log(buffer);
             sharp(new Uint8ClampedArray(buffer.buffer)).toFile(returnFileName)
             .then(info=>{
-                setInfo(info)
-            }
-                
-            );
+                //console.log(info)
+                setInfo(prevInfo => ({...prevInfo, file: info}))
+            })
+            .catch(e=>{console.log(e)})
             //info contains
             //format, width, height, size
             
         }
     )
+    increaseProgress()
 }
