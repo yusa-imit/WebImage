9922cb795b6b1cbd507d49bba7f719c16dbf74d6
yusa-imit <esimileiar81@gmail.com>
2021-09-16 22:51:32 +0900

    imgconverter implementation

--- src/UI/Mainframe/ImageToWebp.jsx
+++ src/UI/Mainframe/ImageToWebp.jsx
@@ -5,22 +5,28 @@ import { sharpConvertAndExport } from './sharpFunction.js'
 import ButtonComp from './Component/ButtonComp.jsx'
 import Scrollbars from 'react-custom-scrollbars'
 import ImageViewer from './Component/ImageViewer.jsx'
+import FileNameComp from './Component/FileNameComp.jsx'
+import { Progress } from 'react-sweet-progress';
+import "react-sweet-progress/lib/style.css";
 
 export default function ImageToWebp(props) {
     const [info, setInfo] = useState([]);
-    const [convertInto, setConvertInto] = useState('WEBP');
-    const changeInfo = (num) => {
-
-    }
+    const [convertInto, setConvertInto] = useState('webp');
     //Get Remote for dialog
     const dialog = require('electron').remote.dialog;
     const availableFormat = ['jpg', 'jpeg', 'png', 'webp', 'avif', 'tiff', 'gif', 'heif']
-    const buttons = ['JPEG', 'PNG', 'WEBP', 'AVIF', 'TIFF', 'GIF', 'HEIF']
-    const setButtons = () =>{
+    const buttons = [{ 'title': 'JPEG', 'data': 'jpeg' },
+    { 'title': 'PNG', 'data': 'png' },
+    { 'title': 'WEBP', 'data': 'webp' },
+    { 'title': 'AVIF', 'data': 'avif' },
+    { 'title': 'TIFF', 'data': 'tiff' },
+    { 'title': 'GIF', 'data': 'gif' },
+    { 'title': 'HEIF', 'data': 'heif' }]
+    const setButtons = () => {
         return (
-            buttons.map((button)=>(
-                    <ButtonComp text={button} key={button} backgroundColor={button===convertInto?'#dc143c':'transparent'} textColor={'#fdf5e6'} handleClick={() => {setConvertInto(button)}} />
-        ))
+            buttons.map((button) => (
+                <ButtonComp text={button.title} key={button.title} backgroundColor={button.data === convertInto ? '#dc143c' : 'transparent'} textColor={'#fdf5e6'} handleClick={() => { setConvertInto(button.data) }} />
+            ))
         );
     }
     const formatConfirm = (f) => {
@@ -34,56 +40,72 @@ export default function ImageToWebp(props) {
     }
     const [drag, setDrag] = useState(false);
     const [progress, setProgress] = useState(0);
+    const increaseProgress = () =>{
+        setProgress(prevProg => prevProg+1);
+    }
     const [files, setFiles] = useState([]);
     //Only Image Variables
     const fileFilter = [{ name: 'Images', extensions: availableFormat }]
     //function getFile = On get file Button Event Listner
     const getFile = () => {
         dialog.showOpenDialog({ filters: fileFilter, properties: ['openFile', 'multiSelections', 'showHiddenFiles',] })
-        .then(
-            result => {
-                if (result.canceled === true) {                    
-                    setFiles([])
-                }
-                else {
-                    for (var file of result.filePaths) {
-                        if(files.includes(file))
+            .then(
+                result => {
+                    if (result.canceled === true) {
+                        
+                    }
+                    else {
+                        for (var file of result.filePaths) {
+                            if (files.includes(file))
                                 continue;
-                        if (formatConfirm(file)) {
-                            setFiles(prevfiles=>[...prevfiles, file]);        
+                            if (formatConfirm(file)) {
+                                setFiles(prevfiles => [...prevfiles, file]);
+                            }
                         }
                     }
                 }
-            }
-        )
-        .catch(e=>{
-            console.log(e);
-        })
+            )
+            .catch(e => {
+                console.log(e);
+            })
     }
-    const customDropEventListener=(e)=>{
-        e.preventDefault();
-        e.stopPropagation();
-        for(const f of e.dataTransfer.files){
-            func(f.path);
+    const [target, setTarget] = useState('Default');
+    const getTargetDirectory = () => {
+        const dir = dialog.showOpenDialogSync({
+            properties: ['openDirectory']
+        })
+        if (dir === undefined) {
+            setTarget('Default');
+        }
+        else {
+            setTarget(dir);
         }
     }
-    
-    const convert = () => {
-        sharpConvertAndExport('C:\\Users\\esimi\\Downloads\\E8guACLVkAQV6-j.jpeg', 'webp', setInfo)
+    const convert = (f) => {
+        sharpConvertAndExport(f, convertInto, target, setInfo, increaseProgress)
+    }
+    const convertAll = () => {
+        for (var file of files) {
+            convert(file);
+        }
+    }
+    const initialize = () => {
+        setInfo([]);
+        setFiles([]);
+        setProgress(0);
     }
     const scroll = useRef();
-    const detector = useRef();
-    
+    useEffect(() => { console.log(progress)}, [files, progress])
     return (
         <>
-            <div className='mainframe-wrapper' onClick={() => { convert() }}>
+            <div className='mainframe-wrapper'>
                 <div className='webp-wrapper'>
-                    <div className='webp-image-div' onClick={() => { getFile() }}>
+                    <div className='webp-image-div' onClick={() => { }}>
                         <Scrollbars ref={scroll} autoHide={true} className='webp-scroll'>
-                            <div  className='webp-image-container'>
+                            <div className='webp-image-container'>
                                 {
-                                    files.map((file, index)=>(
-                                        <ImageViewer file={file} key={index}/>
+                                    files.map((file, index) => (
+                                        <ImageViewer file={file} key={index} />
                                     ))
                                 }
                             </div>
@@ -91,57 +113,69 @@ export default function ImageToWebp(props) {
                     </div>
                     <div className='webp-button-wrapper'>
 
-                            <ButtonComp text={'Add File'} backgroundColor={'#3cb712'} textColor={'#fdf5e6'} handleClick={() => { getFile() }} />
+                        <ButtonComp text={'Add File'} backgroundColor={'#3cb712'} textColor={'#fdf5e6'} handleClick={() => { getFile() }} />
 
                         {
                             setButtons()
                         }
                     </div>
-                    <div className='webp-progress-bar'>
+                    <div className='webp-result'>
+                        <div className='target-dest'>
+                            <FileNameComp header={'Result Destination'} fileName={target} onClick={() => { getTargetDirectory() }} />
+                        </div>
+                        <div className='progress-bar'>
+                            <h5 style={{ marginRight: '20px' }}>Progress</h5>
+                            <Progress symbolClassName={'progress-bar-comp'} percent={files.length === 0 ? 0 : ((progress / files.length) * 100).toFixed(0)} theme={{
+                                active: {
+                                    color: '#fdf5e6',
+                                }
+                            }} />
+                        </div>
+                        <div className='convert-button'>
+                            <div className='button-wrapper'>
+                                <ButtonComp text={'RESET FILE'} backgroundColor={'#3cb712'} textColor={'#fdf5e6'} handleClick={() => { initialize() }} />
+                            </div>
+                            <div className='button-wrapper'>
+                                <ButtonComp text={'CONVERT'} backgroundColor={'#dc143c'} textColor={'#fdf5e6'} handleClick={() => { convertAll() }} />
+                            </div>
 
+
+                        </div>
                     </div>
                 </div>
-                <div className='drag-and-drop-area' ref={detector}
+                <div className='drag-and-drop-area'
                     onDrop={(e) => {
                         dropEventListener(e, (f) => {
+                            
                             if (isDirectory(f)) {
                                 var dirFiles = getDirectoryFiles(f);
+                                var fileArr = [];
                                 for (var file of dirFiles) {
-                                    console.log(file)
-                                    if(files.includes(f+'\\'+file)){
+                                    if(files.includes(file))
                                         continue;
-                                    }
+                                    if(fileArr.includes(file))
+                                        continue;
                                     if (formatConfirm(file)) {
-                                        setFiles([...files, f + '\\' + file]);
+                                        fileArr.push(f+'\\'+file);
                                     }
                                 }
+                                setFiles(prevfiles => [...prevfiles, ...fileArr]);
                             }
                             else {
                                 if (formatConfirm(f)) {
-                                    if(files.includes(f))
-                                            return;
+                                    if (files.includes(f))
+                                        return;
                                     setFiles([...files, f]);
                                 }
                             }
-                            /*
-                            var extension = p.split('.')[p.split('.').length - 1];
-                            var acceptables = ['jpg', 'jpeg', 'png', 'gif'];
-                            if (!acceptables.includes(extension)) {
-                                setDrag(false);
-                                setError(true);
-                                initialize();
-                                return;
-                            }
-                            setDrag(false);
-                            getFile(p);*/
                         })
                     }}
                     onDragOver={dragOverListener}
                     onDragEnter={(e) => { dragEnterListener(e, () => { setDrag(true) }) }}
                     onDragLeave={(e) => { dragLeaveListner(e, () => { setDrag(false) }) }}
-                    onClick={() => {console.log("click")}}
-                    
-                    onWheel={(e)=>{scroll.current.scrollTop(scroll.current.getScrollTop()+e.deltaY)}}
+                    onClick={() => {  }}
+
+                    onWheel={(e) => { scroll.current.scrollTop(scroll.current.getScrollTop() + e.deltaY) }}
                 >
 
                 </div>
@@ -159,50 +193,3 @@ function getDirectoryFiles(f) {
     var fs = require('fs');
     return fs.readdirSync(f);
 }
-
-// Dummy for file converting
-/*
-    sharpConvert('C:\\Users\\esimi\\Downloads\\E8guACLVkAQV6-j.jpeg', 'webp').then(
-        buffer=>{
-            console.log(buffer)
-            //const pixelArray = new Uint8ClampedArray(buffer);
-            //console.log(pixelArray)
-            const url = imageConversion.filetoDataURL(new Blob([buffer])).then(
-                data=>{
-                    setImg(data)
-                    console.log(data)
-                }
-            )
-            sharp(new Uint8ClampedArray(buffer.buffer)).toFile('C:\\Users\\esimi\\Downloads\\E8guACLVkAQV6-j.webp').then(info=>{console.log(info)});
-            //info contains
-            //format, width, height, size
-        }
-    )
-    */
-
-
-    function ImageSetter(props){
-        const files = props.files;
-        const setImageComp = () =>{
-            if(files === []){
-                return (
-                    <>
-                    </>
-                );
-            }
-            else{
-                console.log('files::::')
-                console.log(files[0])
-                return(
-                    files.map((file)=>(
-                        <ImageViewer file={file}/>
-                    ))
-                )
-            }
-        }
-        return(
-            <>
-            {setImageComp()}
-            </>
-        )
-    }
