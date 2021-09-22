4c2016bda6539aee41072949571bc1069542717e
yusa-imit <esimileiar81@gmail.com>
2021-09-22 18:47:52 +0900

    Loading Circle added

---
+++ src/UI/Mainframe/Component/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Loading.css';
+
+export default function Loading(props){
+    return(
+        <>
+            <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </>
+    )
+}
