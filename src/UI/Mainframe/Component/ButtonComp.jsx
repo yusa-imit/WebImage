d1e241a3482382fef219e9a9b9c7d90ae4774326
yusa-imit <esimileiar81@gmail.com>
2021-09-28 20:10:28 +0900

    button component regularization

--- src/UI/Mainframe/Component/ButtonComp.jsx
+++ src/UI/Mainframe/Component/ButtonComp.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './ButtonComp.css';
 
 export default function ButtonComp(props){
-    const bg = props.backgroundColor===undefined?'transparent':props.backgroundColor;
-    const textColor = props.textColor===undefined?'#000000':props.textColor;
+    const disable = props.disable===undefined?false:props.disable;
+    const bg = props.backgroundColor===undefined?'transparent':disable===false?props.backgroundColor:'#3B3B3B';
+    const textColor = props.textColor===undefined?'#000000':disable===false?props.textColor:'#868686';
     return(<>
-        <div className='button-comp' style={{backgroundColor:bg, color:textColor}} onClick={()=>{props.handleClick()}}>
+        <div className='button-comp' style={{backgroundColor:bg, color:textColor}} onClick={()=>{if(disable){return}; props.handleClick()}}>
             <div className='button-comp-text'>
                 <h4>
                     {props.text}
