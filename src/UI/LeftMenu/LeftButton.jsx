716506875dd04bae450c4020caec17be9c68ad6b
yusa-imit <esimileiar81@gmail.com>
2021-09-28 23:50:00 +0900

    Leftbutton implementation

--- src/UI/LeftMenu/LeftButton.jsx
+++ src/UI/LeftMenu/LeftButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './LeftButton.css'
 
@@ -12,24 +12,47 @@ export default function LeftButton(props){
     // State : click
     // set true when this button component is clicked
     const [click, setClick] = useState(false);
-    const setClass = () =>{
-        if(click){
-            return 'LeftButton-wrapper-inset'
+    const setTextClass = () =>{
+        if(props.index===props.buttonState){
+            return 'LeftButton-text-clicked';
         }
         else{
-            return 'LeftButton-wrapper'
+            return 'LeftButton-text';
         }
         
     }
+    const setBackgroundColor = () =>{
+        if(props.index===props.buttonState){
+            return 'LeftButton-background-clicked';
+        }
+        else{
+            return 'LeftButton-background';
+        }
+    }
+    const setWrapperStyle = () =>{
+        if(props.index===props.buttonState){
+            return {borderBottom:'1px solid #222', borderLeft:'1px solid #222'};
+        }
+        else{
+            return {};
+        }
+    }
+    const animation = useRef()
     return(
         <>
-            <div className={"LeftButton-wrapper"} onClick={()=>{props.changeButtonState(props.index)}}>
-                
-                <span className='LeftButton-text'>
+            <div className={"LeftButton-wrapper"} style={setWrapperStyle()} onClick={()=>{props.changeButtonState(props.index)}}
+                onMouseOver={(e)=>{animation.current.style.left = e.pageX -e.currentTarget.offsetLeft + "px"; animation.current.style.top = e.pageY - e.currentTarget.offsetTop + "px";}}
+                onMouseOut={(e)=>{animation.current.style.left = e.pageX - e.currentTarget.offsetLeft+ "px"; animation.current.style.top = e.pageY - e.currentTarget.offsetTop+ "px";}}
+            >
+                <div ref={animation} 
+                />
+                <span className={setTextClass()}>
                     <FontAwesomeIcon className='LeftButton-icon' icon={props.source.icon}/>
-                    <span>{props.source.name}</span>
-                    </span>
+                    <text>{props.source.name}</text>
+                </span>
+                <span className={setBackgroundColor()}></span>
             </div>
+            
         </>
     )
 }
