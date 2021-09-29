ec4a04fe7ef45e38e043dfa8f48b74d03e1be4b6
yusa-imit <esimileiar81@gmail.com>
2021-09-29 19:56:35 +0900

    webpack rule change for json

--- webpack.rules.js
+++ webpack.rules.js
@@ -39,19 +39,6 @@ module.exports = [
       }
     }
   },
-  {
-    test: /\.py$/,
-    use:{
-      loader: 'py-loader',
-      options: {
-        compiler: 'transcrypt'
-      }
-    }
-  },
-  {
-    test : /\.json$/,
-    loader : "json-loader"
-  },
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
   /**
