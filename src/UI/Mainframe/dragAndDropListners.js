f7038edc589ed737e9742120b013e55477cd8848
yusa-imit <esimileiar81@gmail.com>
2021-10-01 16:19:45 +0900

    finalize drag and drop listeners

--- src/UI/Mainframe/dragAndDropListners.js
+++ src/UI/Mainframe/dragAndDropListners.js
@@ -1,22 +1,26 @@
+// calls when drop event is triggered
 export const dropEventListener=(e, func=()=>{})=>{
     e.preventDefault();
     e.stopPropagation();
     for(const f of e.dataTransfer.files){
-        console.log(f.path)
         func(f.path);
     }
 }
 
+
+// calls when drag over event is triggered
 export const dragOverListener=(e)=>{
     e.preventDefault();
     e.stopPropagation();
 }
+
+// calls when drag enter event is triggered
 export const dragEnterListener=(e, f=()=>{})=>{
-    console.log('file is in the Drop Space');
     f();
 }
 
+
+// calls when drag leaves without doing nothing
 export const dragLeaveListner=(e, f=()=>{})=>{
-    console.log('file has left the Drop Space');
     f();
 }
